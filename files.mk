8f6fec3352df8d69406ef9191a8d70b761d5a318
David Broman <broman@eecs.berkeley.edu>
2014-01-07 10:11:24 -0800

    Added .gitignore

--- files.mk
+++ files.mk
@@ -1,9 +1,9 @@
 
-LIB_NAME = utools
+LIB_NAME = ucaml
 FILES = utils ustring utest ugraph 
 TEST_FILES = testUtils testGraph 
 ONLY_MLI_FILES = 
 INCLUDE_DIR = 
 DEP_LIBS = 
-UNITTEST_DIR = utest
+UNITTEST_DIR = unittest
 
