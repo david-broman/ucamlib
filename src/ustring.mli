886367748ac9984144cef0e2e05ad91c72a8cfa2
David Broman <broman@eecs.berkeley.edu>
2014-01-25 09:29:39 -0800

    Fixed documentation typo.

--- src/ustring.mli
+++ src/ustring.mli
@@ -361,7 +361,7 @@ val empty : unit -> ustring
 val split : ustring -> uchar -> ustring list
 (** [split s c] splits string [s] at points where character [c] appears. 
     For instance, if string ["This is a string"] is split using the space
-    character ' ', the return list is [\["This";"is";"a";"string"\]] *)
+    character [' '], then list [\["This";"is";"a";"string"\]] is returned.*)
 
 val unix2dos : string -> string 
 (** Function [Ustring.unix2dos s] returns a string where newline characters in 
